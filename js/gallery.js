298c6adc2b958b98db7c3ad38993d9280cef1ec4
Soonkai97 <chianraymond97@gmail.com>
2020-07-23 17:42:09 +0800

    ok

--- js/gallery.js
+++ js/gallery.js
@@ -1,35 +1,36 @@
 let galleryImages = document.querySelectorAll(".gallery-img");
 let getLatestOpenedImg;
 let sk;
-let windowWidth=window.innerWidth;
+let windowWidth = window.innerWidth;
+let wy;
 
-if(galleryImages){
-    galleryImages.forEach(function(image,index){
-        image.onclick =function(){
-            let getElementCss=window.getComputedStyle(image);
-            let getFullImgUrl=getElementCss.getPropertyValue("background-image");
+if (galleryImages) {
+    galleryImages.forEach(function(image, index) {
+        image.onclick = function() {
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
             let getImgUrlPos = getFullImgUrl.split("/img/thumbs/");
 
-            let setNewImgUrl=getImgUrlPos[1].replace('")','');
+            let setNewImgUrl = getImgUrlPos[1].replace('")', '');
 
-            getLatestOpenedImg= index +1;
+            getLatestOpenedImg = index + 1;
 
-            let container=document.body;
-            let newImgWindow=document.createElement("div");
+            let container = document.body;
+            let newImgWindow = document.createElement("div");
             container.appendChild(newImgWindow);
-            newImgWindow.setAttribute("class","img-window");
+            newImgWindow.setAttribute("class", "img-window");
 
             newImgWindow.setAttribute("onclick", "closeImg()");
 
             let newImg = document.createElement("img");
             newImgWindow.appendChild(newImg);
-            newImg.setAttribute("src", "/img/"+setNewImgUrl);
+            newImg.setAttribute("src", "/img/" + setNewImgUrl);
             newImg.setAttribute("id", "current-img");
 
-            newImg.onload = function(){
+            newImg.onload = function() {
                 let imgWidth = this.width;
                 // this refers to newimg
-                let calcImgToEdge= ((windowWidth - imgWidth)/ 2)- 80;
+                let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
 
 
                 let newPrevBtn = document.createElement("a");
@@ -37,8 +38,8 @@ if(galleryImages){
                 newPrevBtn.appendChild(btnPrevText);
                 container.appendChild(newPrevBtn);
                 newPrevBtn.setAttribute("class", "img-btn-prev");
-               newPrevBtn.setAttribute("onclick", "changeImg(0)");
-                newPrevBtn.style.cssText ="left: "+calcImgToEdge+"px;";
+                newPrevBtn.setAttribute("onclick", "changeImg(0)");
+                newPrevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
 
                 let newNextBtn = document.createElement("a");
                 let btnNextText = document.createTextNode("next");
@@ -48,8 +49,8 @@ if(galleryImages){
                 newNextBtn.setAttribute("onclick", "changeImg(1)");
                 newNextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
             }
-            
-           
+
+
 
 
 
@@ -57,13 +58,14 @@ if(galleryImages){
     });
 }
 
-function closeImg(){
+function closeImg() {
     document.querySelector(".img-window").remove();
     document.querySelector(".img-btn-prev").remove();
     document.querySelector(".img-btn-next").remove();
-    
+
 }
-function changeImg(changeDir){
+
+function changeImg(changeDir) {
     document.querySelector("#current-img").remove();
 
     let getImgWindow = document.querySelector(".img-window");
@@ -73,37 +75,36 @@ function changeImg(changeDir){
 
     let calcNewImg;
 
-    if(changeDir === 1){
-        calcNewImg = getLatestOpenedImg +1;
-        if(calcNewImg>galleryImages.length){
-            calcNewImg=1;
+    if (changeDir === 1) {
+        calcNewImg = getLatestOpenedImg + 1;
+        if (calcNewImg > galleryImages.length) {
+            calcNewImg = 1;
 
 
         }
-    }
-    else if(changeDir ===0)
+    } else if (changeDir === 0)
         calcNewImg = getLatestOpenedImg - 1;
-    if (calcNewImg <1) {
+    if (calcNewImg < 1) {
         calcNewImg = galleryImages;
 
 
     }
 
-    newImg.setAttribute("src","/img/" +calcNewImg +".jpg");
+    newImg.setAttribute("src", "/img/" + calcNewImg + ".jpg");
     newImg.setAttribute("id", "current-img");
-    getLatestOpenedImg =calcNewImg;
+    getLatestOpenedImg = calcNewImg;
 
-    newImg.onload =function(){
-        let imgWidth=this.width;
+    newImg.onload = function() {
+        let imgWidth = this.width;
         let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
 
         let nextBtn = document.querySelector(".img-btn-next");
 
 
         let prevBtn = document.querySelector(".img-btn-prev");
-        
+
         nextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
-        
+
         prevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
     }
 
